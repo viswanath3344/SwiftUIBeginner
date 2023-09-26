15b64058e0251b12f908226b7a56532810f4c03b
Viswanath <viswanath3344@gmail.com>
2023-09-26 15:26:51 +0530

    Initial Commit

---
+++ SwiftUIBeginner/SwiftUIBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBeginnerApp.swift
+//  SwiftUIBeginner
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
